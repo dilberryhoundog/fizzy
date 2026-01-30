ceebd17aa6409102f9ed124c1f6261603dfbfe0e
Dylan <dylan@restaurantcare.com.au>
2026-01-30 23:19:31 +1030

    🔧 chore: set Active Storage service URLs to expire in 2 weeks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,4 +121,6 @@ Rails.application.configure do
 
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.active_storage.service_urls_expire_in = 2.weeks
 end
